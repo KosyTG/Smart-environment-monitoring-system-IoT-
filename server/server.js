2a1e9c07007929087e5a5cec8f8515387c3ddec8
emmanuel <emmanuelkosi19@gmail.com>
2026-04-22 04:27:23 +0100

    Fix CORS to allow all origins

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: '*',
   methods: ['GET', 'POST'],
-  credentials: true
+  credentials: false
 }));
 app.use(express.json());
 app.use(morgan('dev'));
